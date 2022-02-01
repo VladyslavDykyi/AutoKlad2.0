ed63a47b5a89441865077eaad6060091bf7d120c
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2022-02-01 17:16:29 +0200

    upd 01.02.2022

--- #src/js/pages/slider.js
+++ #src/js/pages/slider.js
@@ -57,5 +57,19 @@ $(document).ready(function () {
 
 	$('[data-fancybox="gallery"]').fancybox({
 		// Options will go here
+		baseTpl:
+			'<div class="fancybox-container" role="dialog" tabindex="-1">' +
+			'<div class="fancybox-bg"></div>' +
+			'<div class="fancybox-inner">' +
+			'<div class="fancybox-infobar"><span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span></div>' +
+			'<div class="fancybox-toolbar">{{buttons}}</div>' +
+			'<div class="fancybox-navigation">{{arrows}}</div>' +
+			'<div class="fancybox-stage"></div>' +
+			'<div class="fancybox-caption"><div class="fancybox-caption__body"></div>' +
+			'<div class="fancybox-caption__body2">Картинка носит ознакомительный характер и может отличаться от реального товара</div>' +
+			'</div>' +
+			'</div>' +
+			'</div>',
 	});
+	$('.slick-cloned div a').removeAttr('data-fancybox');
 });
